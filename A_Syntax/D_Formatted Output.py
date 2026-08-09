cc0922019f01260ad26323d3bb7ab203a925d6bc
李彬華 <phlee0119@gmail.com>
2026-08-10 00:26:36 +0800

    Formatted Output

---
+++ A_Syntax/D_Formatted Output.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  9 16:56:24 2026
+
+@author: User
+"""
+
+tempC = eval( input("請輸入攝氏溫度值 : ") )
+
+tempF = tempC * ( 9 / 5 ) + 32
+
+print("攝氏溫度", tempC, "度，相當於華氏溫度為", tempF, "度")
+
+print("攝氏溫度 {} 度，相當於華氏溫度為 {} 度".format( tempC, tempF ))     # 格式化輸出 str.format()
+
+
+
+intA = 3
+intB = 5
+
+result = "{} * {} = {}".format( intA, intB, intA * intB )
+
+print( result )
+
+
+
+intC = 123
+print( "華氏溫度{}度".format( intC ) )
+print( "華氏溫度{:5}度".format( intC ) )
+print( "華氏溫度{:5d}度".format( intC ) )
+print( "華氏溫度{:>5d}度".format( intC ) )
+print( "華氏溫度{:<5d}度".format( intC ) )
+print( "華氏溫度{:^5d}度".format( intC ) )
+
+
+
+intD = 123.45678
+print( "華氏溫度{:.3f}度".format( intD ) )
+print( "華氏溫度{:+.3f}度".format( intD ) )
+print( "華氏溫度{:.0f}度".format( intD ) )
+print( "華氏溫度{:10.3f}度".format( intD ) )
+
+print( "華氏溫度{:x>10.3f}度".format( intD ) )
+print( "華氏溫度{:x<10.3f}度".format( intD ) )
+
+
+
+intE = 1234.5678
+print( "華氏溫度{:,}度".format( intE ) )
+print( "華氏溫度{:.2%}度".format( intE ) )
+print( "華氏溫度{:.2e}度".format( intE ) )
