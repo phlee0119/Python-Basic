3ed6ceea50be11a3f934dc9c9625057d3ddf5727
李彬華 <phlee0119@gmail.com>
2026-08-09 16:29:59 +0800

    Temperature

---
+++ B_Example/2_Temperature.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Aug 9 2026
+
+@author: Lee, Pin-Hua
+"""
+
+tempC = eval( input("請輸入攝氏溫度值 : ") )
+
+tempF = tempC * ( 9 / 5 ) + 32                # 華氏 = 攝氏 * ( 9 / 5 ) + 32
+
+print("攝氏溫度", tempC, "度，相當於華氏溫度為", tempF, "度")
